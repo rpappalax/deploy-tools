af2979643fd8f2f7aeb3bcec992c683bf9790eb8
Richard Pappalardo <rpappalax@gmail.com>
2016-09-20 16:24:50 -0700

    add autopush scripts

---
+++ autopush/watch.sh
@@ -0,0 +1,28 @@
+source config.sh
+
+URL_STATUS="https://$HOST_UPDATES/status"
+clear; 
+JSON=`curl -s "$URL_STATUS"`
+VERS_OLD=`echo $JSON | /usr/local/bin/jq '.version' ;` 
+say $VERS_OLD
+while :; do 
+   clear; 
+   JSON=`curl -s "$URL_STATUS"`
+   VERS=`echo $JSON | /usr/local/bin/jq '.version' ; sleep 5;` 
+   if [ "$VERS_OLD" == "$VERS" ]; then
+      MSG="still same version: $VERS"
+      FLAG=0
+   else
+      MSG="Attention!  Attention! Attention! Attention! DNS has changed! New version is: $VERS"
+      FLAG=1
+   fi
+   echo $MSG 
+   say $MSG
+   if [ $FLAG == 1 ]; then
+        echo $MSG 
+        break;
+   fi
+echo $MSG 
+say $MSG
+
+done
